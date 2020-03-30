bab124ca552ae87abda782360d3f0a13556241b9
brandonstiles1 <brandonstiles@gmail.com>
2020-03-30 10:12:57 -0400

    Homepage working

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Styling
+import './homepage.styles.scss';
+
+// Components
+import Directory from '../../components/directory/directory.component';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+)
+
+export default HomePage;
