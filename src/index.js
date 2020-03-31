ae8a52fc3f2dcbf3cb38c1e0ca63f22a8674938d
brandonstiles1 <brandonstiles@gmail.com>
2020-03-31 18:36:26 -0400

    Implemented Redux

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={ store }>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
