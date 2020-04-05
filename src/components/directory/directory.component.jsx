3f2fbb140bf46dd6bfa78c8e9eafa394a5ed4004
brandonstiles1 <brandonstiles@gmail.com>
2020-04-05 15:10:34 -0400

    Added reducer for directory

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -4,63 +4,23 @@ import './directory.styles.scss';
 // Components
 import MenuItem from '../menu-item/menu-item.component';
 
+// Redux
+import { connect } from 'react-redux';
+import { selectDirectorySections } from '../../redux/directory/directory.selector';
+import { createStructuredSelector } from 'reselect';
 
-
-class Directory extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      sections: [
-        {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1,
-          linkUrl: 'hats'
-        },
-        {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2,
-          linkUrl: ''
-        },
-        {
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3,
-          linkUrl: ''
-        },
-        {
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          size: 'large',
-          id: 4,
-          linkUrl: ''
-        },
-        {
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          id: 5,
-          size: 'large',
-          linkUrl: ''
-        }
-      ]
+const Directory = ({ sections }) => (
+  <div className='directory-menu'>
+    {
+      sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem key={ id } { ...otherSectionProps } />
+      ))
     }
+  </div>
+);
 
-  }
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+})
 
-  render () {
-    return (
-      <div className='directory-menu'>
-        {
-          this.state.sections.map(({ id, ...otherSectionProps }) => (
-            <MenuItem key={ id } { ...otherSectionProps } />
-          ))
-        }
-      </div>
-
-    );
-  }
-}
-
-export default Directory;
+export default connect(mapStateToProps)(Directory);
